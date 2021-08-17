bfd1ca3ff733bb6c56dc84ccedb03bdd5c06d15b
Bruno Viveiros <bruferviveiros@gmail.com>
2021-08-17 09:56:17 -0300

    Utilizando a Tab Navigation

--- App.tsx
+++ App.tsx
@@ -3,15 +3,16 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { AppRoutes } from './src/routes/app.routes';
+
 import {
   useFonts,
   Poppins_400Regular,
   Poppins_500Medium,
   Poppins_700Bold,
 } from '@expo-google-fonts/poppins';
-
-import { Register } from './src/screens/Register';
-
 import theme from './src/global/styles/theme';
 
 export default function App() {
@@ -27,7 +28,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
